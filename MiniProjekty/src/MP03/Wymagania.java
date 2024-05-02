18632b9ca3889315f0f3deb916051c20edbebcc7
KingaMendyk <s25275@pjwstk.edu.pl>
2024-05-02 11:36:27 +0200

    Create Wymagania.java

---
+++ MiniProjekty/src/MP03/Wymagania.java
@@ -0,0 +1,11 @@
+package MP03;
+
+public class Wymagania {
+    /*
+    Klasa abstrakcyjna i polimorficzne wołanie metod
+    Overlapping
+    Wielodziedziczenie
+    Wieloaspektowe
+    Dynamiczne
+     */
+}
