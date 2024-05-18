66f8d746cb0b932532274d23fe6433264f4cfc95
KingaMendyk <s25275@pjwstk.edu.pl>
2024-05-18 12:06:25 +0200

    Static attribute constraint

--- MiniProjekty/src/MP04/Worker.java
+++ MiniProjekty/src/MP04/Worker.java
@@ -5,7 +5,11 @@ public class Worker extends Person {
     private static int latestId;
     private Hotel hotel; //Asocjacja kwalifikowana
     private int salary;
+    private int workHours; //Ograniczenie statyczne atrybutu
+    private static int maxWorkHours;
+    private static int minWorHours;
     private Person person;
+    private WorkerType workerType;
 
     public Worker(int id, String name, String surname) {
         super(name, surname, PersonType.Worker);
@@ -53,6 +57,30 @@ public class Worker extends Person {
 
     public void setPerson(Person person) {
         this.person = person;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getWorkHours() {
+        return workHours;
+    }
+
+    public void setWorkHours(int workHours) { //Ograniczenie statyczne atrybutu
+        if(workHours < minWorHours){
+            System.out.println("Work hours must be at least " + minWorHours + "!");
+            return;
+        }
+        if(workHours > maxWorkHours){
+            System.out.println("Work hours must not exceed " + maxWorkHours + "!");
+            return;
+        }
+        this.workHours = workHours;
     }
 
     @Override
---
+++ MiniProjekty/src/MP04/WorkerType.java
@@ -0,0 +1,6 @@
+package MP04;
+
+public enum WorkerType {
+    Staff,
+    Manager
+}
