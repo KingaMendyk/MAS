b07b56fd2f6d695b59928962e03b1725360fefc4
KingaMendyk <s25275@pjwstk.edu.pl>
2024-05-22 13:21:27 +0200

    Create Wymagania.java

---
+++ MiniProjekty/src/MP05/Wymagania.java
@@ -0,0 +1,13 @@
+package MP05;
+
+public class Wymagania {
+    /*
+        MR - klasy
+        MR – asocjacje (1:* lub *:*)
+        MR - dziedzicz
+
+        Należy stworzyć działający program korzystający z bazy danych i realizujący
+        powyższe konstrukcje. Można korzystać z dowolnych narzędzi, m. in. ORM-ów, np.
+        Hibernate, Entity Framework.
+     */
+}
