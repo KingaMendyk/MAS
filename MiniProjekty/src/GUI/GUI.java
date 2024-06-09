8c8fc96602e907f610b78c554052b264e1f83d82
KingaMendyk <s25275@pjwstk.edu.pl>
2024-06-09 22:20:07 +0200

    Added main panel and animal panel

--- MiniProjekty/src/GUI/GUI.java
+++ MiniProjekty/src/GUI/GUI.java
@@ -1,4 +1,42 @@
 package GUI;
 
+import javax.swing.*;
+
 public class GUI {
+    private JFrame mainFrame;
+    private JPanel mainPanel;
+    private JPanel mainViewPanel;
+    private JPanel bannerPanel;
+    private JPanel lowePanel;
+    private JPanel imagePanel;
+    private JPanel buttonsPanel;
+    private JButton showAnimalsButton;
+    private JButton reserveRoomButton;
+    private JLabel mainLabel;
+    private JPanel animalsViewPanel;
+    private JLayeredPane layeredPane1;
+    private JLabel animalLabel;
+    private JPanel animalBannerPanel;
+    private JPanel animalLowerPanel;
+    private JList animalList;
+
+    public GUI(){
+        mainFrame = new JFrame();
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        mainFrame.add(mainPanel);
+        //mainFrame.add(animalsViewPanel);
+
+        mainViewPanel.setVisible(false);
+        animalsViewPanel.setVisible(true);
+
+
+        mainFrame.setSize(980, 700);
+        mainFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new GUI();});
+    }
 }
